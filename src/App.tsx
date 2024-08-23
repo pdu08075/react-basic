e58da17c94366f2f5f460dbd6fe0ca54986d2420
pdu08075 <pdu08075>
2024-08-23 10:51:15 +0900

    2024.08.23.10

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Example2 from './component_manage/example/Example2';
 import ListRender from './component_manage/example/ListRender';
 import EventComponent from './interaction/EventComponent';
 import StateComponent from './interaction/StateComponent';
+import ForwordingComponent from './interaction/ForwordingComponent';
 
 function App() {
   return (
@@ -21,7 +22,8 @@ function App() {
       {/* <Example2 /> */}
       {/* <ListRender /> */}
       {/* <EventComponent /> */}
-      <StateComponent />
+      {/* <StateComponent /> */}
+      <ForwordingComponent />
 
     </>
   );
