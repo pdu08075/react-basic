f2ed0684732b73c9230cf97ce5d30aa167f5e001
pdu08075 <pdu08075>
2024-09-03 12:26:33 +0900

    2024.09.03.12

--- src/axios/Axios.tsx
+++ src/axios/Axios.tsx
@@ -29,8 +29,23 @@ export default function Axios() {
             console.log(error);
         });
 
+    // option 매개변수
+    // - request에 대한 각종 설정을 조작할 때 사용
+    axios.get('http://3.34.99.196:4000/api/quantification-model', {
+        headers: { Authorization: 'Bearer otsdflsf' },
+        params: { 'time': '1213' }
+    });
 
+    // 주의!! request body가 존재하는 요청에 대해서는 매개변수의 위치 주의
+    axios.post('http://3.34.99.196:4000/api', {}, {
+        headers: { Authorization: 'Bearer otsdflsf' },
+        params: { 'time': '1213' }
+    });
 
+    // CORS
+    // - Cross Origin Resource Sharing
+    // - javascript를 이용한 request 요청 시 출처가 다른 요청에 대하여 자원 공유 정책
+    axios.get('https://naver.com');
 
     return (
         <div>Axois</div>
