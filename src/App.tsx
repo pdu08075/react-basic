88714f6b20c1f1ee808d026ce306f6159df36583
pdu08075 <pdu08075>
2024-09-03 11:55:40 +0900

    2024.09.03.11

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ import PathVariable from './router/PathVariable';
 import PathMove from './router/PathMove';
 import Zustand from './zustand/Zustand';
 import ReactCookie from './react-cookie/ReactCookie';
+import Axios from './axios/Axios';
 
 // react-router 패키지:
 // - react의 SPA(Single Page Application)에서 라우팅을 구현하기 위한 라우팅 어플리케이션
@@ -72,6 +73,7 @@ function App() {
       </Route>
       <Route path='/zustand' element={<Zustand />} />
       <Route path='/react-cookie' element={<ReactCookie />} />
+      <Route path='/axios' element={<Axios />} />
       <Route path='*' element={<h1>404 에러 발생</h1>} />     {/* 지정하지 않은 모든 경로에 대한 처리 */}
 
       {/* <Properties /> */}
